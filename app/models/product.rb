405f19a6e9ebb3b999e8835aa836c82023d40fc5
Kevin Finley <kftwotwo@gmail.com>
2017-02-10 00:20:04 -0800

    add products route

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
-class List < ActiveRecord::Base
+class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :price, :presence => true
   validates :made_from, :presence => true

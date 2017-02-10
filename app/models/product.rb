a00dd29510d4cfe4eaee966e6bcde79e35181846
Kevin Finley <kftwotwo@gmail.com>
2017-02-10 08:58:26 -0800

    show reviews on a product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :price, :presence => true
   validates :made_from, :presence => true
+  validates :picture, :presence => true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,5 +4,6 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :price }
   it { should validate_presence_of :made_from }
+  it { should validate_presence_of :picture }
   it { should have_many :reviews }
 end

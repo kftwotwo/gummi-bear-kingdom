5616184ffa9f206d65179cb9e83f774ebf2b1379
Kevin Finley <kftwotwo@gmail.com>
2017-02-10 15:01:12 -0800

    able to add review

---
+++ spec/features/add_review_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "the add a review process" do
+  it "adds a new product and review" do
+    visit signup_path
+    fill_in 'First name', :with => 'Kevin'
+    fill_in 'Last name', :with => 'Finley'
+    fill_in 'Email', :with => 'candy@candy.com'
+    fill_in 'Pic', :with => 'piture'
+    fill_in 'Password', :with => 'candy'
+    click_on 'Create User'
+    visit new_product_path
+    fill_in 'Name', :with => 'candy'
+    fill_in 'Price', :with => '213'
+    fill_in 'Made from', :with => 'USA'
+    fill_in 'Picture', :with => 'ME'
+    click_on 'Create Product'
+    click_link 'candy'
+    click_link 'New Review'
+    fill_in 'Name', :with => 'candy'
+    fill_in 'Comment', :with => 'good'
+    click_on 'Create Review'
+    expect(page).to have_content 'candy'
+  end
+end
